97701251938269597c94e0edbb678108c76e5b7c
Dist <vlad244244@gmail.com>
2024-09-20 10:09:29 +0300

    added config files

---
+++ fish/config.fish
@@ -0,0 +1,6 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+	export AI_BOT_TOKEN=6977036118:AAG4ZDZ1fjIlbLJEs8WaUkLJMLzJIp-nTGU
+	export TG_BOT_TOKEN=6870699781:AAHLu0HKhuIw3-HAuq-zTcX9N6zXmz0UerY
+	export API_KEY=AIzaSyCI7hv2yJTLyH2Wwh7md8pUEfuVi7E7AXM
+end
