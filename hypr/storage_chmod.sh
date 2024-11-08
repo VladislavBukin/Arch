5cef768a667a1c434a6e43882a60f17258dda327
Dist <vlad244244@gmail.com>
2024-11-08 10:43:14 +0300

    _

--- hypr/storage_chmod.sh
+++ hypr/storage_chmod.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo chgrp -R users /media/storage
-sudo chown -R vlad /media/storage
-sudo chmod -R 755 /media/storage
+sudo chgrp -R users /home/vlad/media/storage
+sudo chown -R vlad /home/vlad/media/storage
+sudo chmod -R 777 /home/vlad/media/storage
--- hypr/xdg-portal-hyprland.sh
+++ hypr/xdg-portal-hyprland.sh
@@ -8,4 +8,6 @@ killall xdg-desktop-portal
 sleep 1
 /usr/libexec/xdg-desktop-portal-hyprland &
 sleep 2
+/usr/libexec/xdg-desktop-portal-gtk &
+sleep 2
 /usr/libexec/xdg-desktop-portal &
